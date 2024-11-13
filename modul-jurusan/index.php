1a74bf5b9fcbbca624c89faf9b918b9645b19dba
rahmadde <teukurahmad120@gmail.com>
2024-11-13 15:18:01 +0700

    Memperbaiki Link Navbar & Membuat Index Jurusan

---
+++ modul-jurusan/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Jurusan</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+    <?php
+        include_once('../navbar.php');
+    ?>
+    <div class="card col-7 mt-5 m-auto row">
+        <div class="card-header">
+             <h3>Data Jurusan</h3>
+        </div>
+        <div class="card_body mt-2 mb-2">
+           <h4>Selamat Datang Pengunjung</h4>
+        </div>
+        <div class="card_body mt-2 mb-2">
+                <p>Walaupun Hidup Tidak Bisa Memberikan Kesenangan, tapi hidup bisa memberikan arti nilai kehidupan.</p>
+        </div>
+    </div>
+
+    <script src="../js/bootstrap.js"></script>
+    <script src="../js/bootstrap.bundle.js"></script>
+</body>
+</html>
