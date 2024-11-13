bc0f4e8174a5d6e20077219a0621016b0f49ce85
rahmadde <teukurahmad120@gmail.com>
2024-11-13 14:50:18 +0700

    Membuat File Index

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project IS62</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+    <?php
+        include_once('navbar.php');
+    ?>
+    <div class="card col-7 mt-5 m-auto row">
+        <div class="card-header text-center">
+             <h3>Sistem Informasi Mahasiswa</h3>
+        </div>
+        <div class="card_body mt-2 mb-2">
+           <h4>Selamat Datang Pengunjung</h4>
+        </div>
+        <div class="card_body mt-2 mb-2">
+                <p>Walaupun Hidup Tidak Bisa Memberikan Kesenangan, tapi hidup bisa memberikan arti nilai kehidupan.</p>
+        </div>
+    </div>
+
+    <script src="js/bootstrap.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</body>
+</html>
